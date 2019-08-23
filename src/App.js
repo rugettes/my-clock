00c278a44b787e7d9d249865fd59f96ff2c8acfc
Rachel <rlense88@gmail.com>
2019-08-22 20:07:20 -0500

    Add timer

--- src/App.js
+++ src/App.js
@@ -7,21 +7,36 @@ class App extends Component {
             <div>
                 <Clock />
             </div>
-        );
+        )
     }
 }
 
 class Clock extends Component {
     state = {
         time: new Date()
-    };
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+    tick() {
+        this.setState({
+            time: new Date()
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>My Clock</h1>
                 <h2>The current time is {this.state.time.toLocaleTimeString()}.</h2>
             </div>
-        );
+        )
     }
 }
 
