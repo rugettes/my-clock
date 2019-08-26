0455e97b33cec1813bf09c2f50a787582b41eb87
Rachel <rlense88@gmail.com>
2019-08-25 20:54:15 -0500

    Update App.js and create Clock.js
    Move Clock code from App to new Clock component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Clock } from './Clock'
 import './App.css'
 
 class App extends Component {
@@ -6,84 +7,6 @@ class App extends Component {
         return (
             <div>
                 <Clock />
-            </div>
-        )
-    }
-}
-
-class Clock extends Component {
-    state = {
-        time: new Date(),
-        power: false,
-        modulation: 'FM'
-    }
-
-    amURL = new Audio('https://nis.stream.publicradio.org/nis.aac')
-    fmURL = new Audio('https://current.stream.publicradio.org/kcmp.aac')
-
-    componentDidMount() {
-        this.timer = setInterval(() => this.tick(), 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer)
-    }
-
-    tick() {
-        this.setState({
-            time: new Date()
-        })
-    }
-
-    togglePower = () => {
-        if (this.state.power) {
-            this.setState({ power: false }, () => {
-                this.amURL.pause()
-                this.fmURL.pause()
-            })
-        } else {
-            this.setState({ power: true }, () => {
-                this.state.modulation === 'AM' ? this.amURL.play() : this.fmURL.play()
-            })
-        }
-    }
-
-    toggleModulation = () => {
-        if (this.state.modulation === 'FM') {
-            this.setState({ modulation: 'AM' }, () => {
-                if (this.state.power) {
-                    this.fmURL.pause()
-                    this.amURL.play()
-                }
-            })
-        } else {
-            this.setState({ modulation: 'FM' }, () => {
-                if (this.state.power) {
-                    this.amURL.pause()
-                    this.fmURL.play()
-                }
-            })
-        }
-    }
-
-    render() {
-        return (
-            <div id="container">
-                <h1>My Clock Radio</h1>
-                <h2>The current time is:</h2>
-                <div id="clock">
-                    <div id="power-toggle" className="button" onClick={ () => { this.togglePower() } }></div>
-                    <div className="spacer-top"></div>
-                    <div id="modulation-toggle" className="button" onClick={() => { this.toggleModulation() }}></div>
-                    <div id="clock-body">
-                        <div className="speaker"></div>
-                        <p id="clock-face">{ this.state.time.toLocaleTimeString() }</p>
-                        <div className="speaker"></div>
-                    </div>
-                    <div className="foot"></div>
-                    <div id="spacer-bottom"></div>
-                    <div className="foot"></div>
-                </div>
             </div>
         )
     }
