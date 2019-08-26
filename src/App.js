0f35553bcb37a28ded2a7e7b9e6720e99a34a817
Rachel <rlense88@gmail.com>
2019-08-25 21:41:14 -0500

    Remove, create, and update files
    Create Clock js and css, Face js and css
    Move relevant js and css from App to Clock and Face
    Delete unecessary App.css
    Update files with Standard js rules

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { Clock } from './Clock'
-import './App.css'
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Clock />
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <Clock />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import { render, unmountComponentAtNode } from "react-dom"
-import { act } from "react-dom/test-utils"
+import { render, unmountComponentAtNode } from 'react-dom'
+import { act } from 'react-dom/test-utils'
 import App from './App'
 
 let container = null
 
 beforeEach(() => {
-    container = document.createElement("div")
-    document.body.appendChild(container)
-});
-
-afterEach(() => {
-    unmountComponentAtNode(container)
-    container.remove()
-    container = null
+  container = document.createElement('div')
+  document.body.appendChild(container)
 })
 
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
 
 it('renders without crashing', () => {
-    act(() => {
-        render(<App />, container)
-    })
+  act(() => {
+    render(<App />, container)
+  })
 })
